e098f79fc02e678e1eadc86ca006ad98119db025
lit dimy <51250902+dimy20@users.noreply.github.com>
2020-04-15 17:32:34 -0300

    Updates

---
+++ precache-manifest.d611f422cfb5951e32538fa6fa0c7f4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfa791db76d479d8d829078c2d4befa",
+    "url": "/chained/index.html"
+  },
+  {
+    "revision": "90f21214228e356f3bc9",
+    "url": "/chained/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1ac8133275ee3c6c471e",
+    "url": "/chained/static/css/main.de23f89f.chunk.css"
+  },
+  {
+    "revision": "90f21214228e356f3bc9",
+    "url": "/chained/static/js/2.32965627.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chained/static/js/2.32965627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac8133275ee3c6c471e",
+    "url": "/chained/static/js/main.12bca564.chunk.js"
+  },
+  {
+    "revision": "9f4ef869817b826d9cc0",
+    "url": "/chained/static/js/runtime-main.c44677f0.js"
+  }
+]);
