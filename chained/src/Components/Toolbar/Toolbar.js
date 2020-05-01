3ce0bda470bad64547536ed5c028fd03adf6caee
lit dimy <51250902+dimy20@users.noreply.github.com>
2020-05-01 05:25:38 -0300

    changed buttons to avatas for testing

--- chained/src/Components/Toolbar/Toolbar.js
+++ chained/src/Components/Toolbar/Toolbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
-import Styles from './Toolbar.module.css'
+import Styles from './Toolbar.module.css';
+import {Avatar} from '@material-ui/core'
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 const Toolbar = ()=>{
     return(
         <div className = {Styles.container}>
@@ -11,11 +13,11 @@ const Toolbar = ()=>{
             </div>
             
             <input className = {Styles.input} placeholder = 'Search' type = 'text'></input>
-            <div >
-            <Button className = {Styles.btn}>Nots</Button>
-            <Button className = {Styles.btn}>chat</Button>{' '}
-            <Button className = {Styles.btn}>profile</Button>{' '}
-            <Button className = {Styles.btn}>conf</Button>
+            <div className = {Styles.btnOptionesContainer}>
+            <Avatar>H</Avatar>
+            <Avatar>H</Avatar>
+            <Avatar>H</Avatar>
+            <Avatar>H</Avatar>
             </div>
             
         </div>
