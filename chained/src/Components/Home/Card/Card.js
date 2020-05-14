0c32ac3345488d5d92ac986cbc3ae3ba4ad92fe9
lit dimy <51250902+dimy20@users.noreply.github.com>
2020-05-14 18:51:33 -0300

    Buttons added -> Guardar,Options - Stylings added => OnHover effects

--- chained/src/Components/Home/Card/Card.js
+++ chained/src/Components/Home/Card/Card.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useRef,useState} from 'react';
 import Styles from './Card.module.css';
-
+import {Button} from '@material-ui/core';
+import {Avatar} from '@material-ui/core'
 const Card = (props)=>{
     const {src} = props;
+    const [IsHovered, setIsHovered] = useState(false);
+    const handleOnMouseOver = ()=>{
+        setIsHovered(true);
+    }
+    const handleOnMouseLeave = ()=>{
+        setIsHovered(false);
+    }
+
     return (
-        <div>
-            <img className = {Styles.container} src = {src} height = '200px'>
-            </img>
+        <div onMouseLeave = {handleOnMouseLeave} onMouseOver= {handleOnMouseOver} className = {Styles.Wrapper}>
+                <img className = {IsHovered ? Styles.imgOnHover : Styles.img} src = {src} height = '200px'>
+                </img>
+                    {IsHovered &&                        
+                        <button className = {Styles.GuardarButtonVisible}>
+                        GUARDAR
+                        </button>  
+                    }
+                    {IsHovered &&
+                        <div>
+                            <button className = {Styles.OptionsButton}>...</button>
+                        </div>
+                    }       
         </div>
     )
 }
