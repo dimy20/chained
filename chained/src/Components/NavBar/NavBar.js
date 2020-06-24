e3053927bd1f18a186929b7994e25f4d5ebfd212
lit dimy <51250902+dimy20@users.noreply.github.com>
2020-06-24 18:23:28 -0300

    xd

--- chained/src/Components/NavBar/NavBar.js
+++ chained/src/Components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -7,7 +7,8 @@ import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Styles from "./NavBar.module.css";
-
+import FetchImagesFromApi from "../FetchImagesFromApi/FetchImagesFromApi";
+import LoadingContext from "../../LoadingContext";
 function TabPanel(props) {
 	const { children, value, index, ...other } = props;
 	const element = <div>{children}</div>;
@@ -32,7 +33,7 @@ TabPanel.propTypes = {
 
 export default function SimpleTabs() {
 	const [CurrentValue, setCurrentValue] = useState(0);
-
+	const [Loading, setLoading] = useState(false);
 	const ref1 = useRef();
 	const ref2 = useRef();
 	const ref3 = useRef();
@@ -66,9 +67,7 @@ export default function SimpleTabs() {
 					id="0"
 					onClick={handleClick}
 					className={Styles.Button}
-				>
-					Todas
-				</div>
+				></div>
 				<div
 					ref={refArr[1]}
 					id="1"
